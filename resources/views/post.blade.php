f0e46cb18735c5f560a4c18353db54ac94e1dd5d
Derek <derekbelloni10@gmail.com>
2022-06-15 15:23:13 -0600

    initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<script src="app.js"></script>
+
+<body>
+  <article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi impedit vero quos commodi, repellendus ab aliquid? Dolorem aliquid ut fugiat quidem ipsum, alias voluptatem suscipit similique quaerat repellat. Sapiente quod, assumenda commodi molestiae fugiat fugit.</p>
+  </article>
+
+  <a href="/">Go Home</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+<script src="app.js"></script>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi impedit vero quos commodi, repellendus ab aliquid? Dolorem aliquid ut fugiat quidem ipsum, alias voluptatem suscipit similique quaerat repellat. Sapiente quod, assumenda commodi molestiae fugiat fugit.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi impedit vero quos commodi, repellendus ab aliquid? Dolorem aliquid ut fugiat quidem ipsum, alias voluptatem suscipit similique quaerat repellat. Sapiente quod, assumenda commodi molestiae fugiat fugit.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi impedit vero quos commodi, repellendus ab aliquid? Dolorem aliquid ut fugiat quidem ipsum, alias voluptatem suscipit similique quaerat repellat. Sapiente quod, assumenda commodi molestiae fugiat fugit.</p>
+    </article>
+</body>
